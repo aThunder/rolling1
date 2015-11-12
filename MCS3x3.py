5e2df971f6ff052a237c8d6150c50a9f718b74ed
aThunder <rr234rr@gmail.com>
2015-11-11 22:46:13 -0500

    deleted several prints used for debugging

--- MCS3x3.py
+++ MCS3x3.py
@@ -14,14 +14,12 @@ def calculate(rounds):
     selected = []
 
     for draw in range(3):
-        # print(bucket)
         counter = 0
         pick = draw_ball(bucket)
         selected.append(pick)
 
         for j in bucket:
          if j == pick:
-            # print("hello", j)
             del bucket[counter]
             break
          counter += 1
@@ -30,15 +28,12 @@ def calculate(rounds):
     # print("selected: ",selected)
 
     if selected[0] == selected[1] == selected[2]:
-        # print("yes")
         match += 1
-        # print(match)
 
   print('matches: ',match)
   print('rounds: ',rounds)
   fraction = match/rounds
   print("fraction of matches: ", fraction)
 
-
-calculate(4200000)
+calculate(200000)
 
