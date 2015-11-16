9c44e692bd3d504feb902afc5a6b2110745fdbc1
aThunder <rr234rr@gmail.com>
2015-11-15 20:15:15 -0500

    condensed the dice rolling into one line and eliminated the dice variables

--- Dropbox/Projects1/Dice/dicetoss2b3.py
+++ Dropbox/Projects1/Dice/dicetoss2b3.py
@@ -11,19 +11,18 @@ class DiceTwo():
       self.rollTotal = rollTotal
                  
       for i in range(self.rounds):
-        dice1 = self.rollDice()
-        dice2 = self.rollDice()
+##        dice1 = self.rollDice()
+##        dice2 = self.rollDice()
         self.counter += 1
-        if dice1 + dice2 == rollTotal:
+##        if dice1 + dice2 == rollTotal:
+        if self.rollDice() + self.rollDice() == rollTotal:
           self.occurence += 1
-          #print(self.occurence)
       self.ratio1 = self.occurence/self.counter
 
    def rollDice(self):
         return random.choice([1,2,3,4,5,6])
 
    def __str__(self):
-      #print('Total No. of Tests: ', self.counter)
       return 'Probability of rolling {0}: {1}'.format(self.rollTotal, self.ratio1)
       return 'Total times {0} resulted in all tests: {1}'.format(self.rollTotal, self.occurence)
 
